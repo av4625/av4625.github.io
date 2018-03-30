a6ac389e19a975dc26bfebcdaab61b4b659f8ced
av4625 <av4625@gmail.com>
2018-03-30 11:50:19 +0100

    New setup on server pi

---
+++ javascript/room_colour.js
@@ -0,0 +1,39 @@
+// Red: #FF232E
+// Green: #57ED69
+// Blue: #0083FF
+
+// Return the hex colour of the room given the temperature
+function generate_room_colour(temperature)
+{
+    var upper_limit = 22;
+    var lower_limit = 20;
+
+    if (temperature > upper_limit)
+    {
+        return '#FF4444';
+    }
+    else if (temperature < lower_limit)
+    {
+        return '#6DC2FF';
+    }
+    else
+    {
+        return '#57ED69';
+    }
+}
+
+function getImage(color)
+{
+    if (color == 'rgb(255, 68, 68)')
+    {
+        return 'images/fire.png';
+    }
+    else if (color == 'rgb(109, 194, 255)')
+    {
+        return 'images/snowflake.png';
+    }
+    else
+    {
+        return '';
+    }
+}
---
+++ php/bedroom.php
@@ -0,0 +1,17 @@
+<?php
+    include 'connection.php';
+
+    // SQL query
+    $query = 'SELECT temperature FROM bedroom ORDER BY date_time DESC LIMIT 1';
+
+    // Execute query
+    $result = mysqli_query($connection, $query);
+
+    $row = mysqli_fetch_row($result);
+
+    // Put the results out encoded as json so I can read it using JavaScript
+    echo $row[0];
+
+    // Close the connection
+    $connection->close();
+?>
