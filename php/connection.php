6c6c52bf79b210f5097c410bca26449be28c0ac6
av4625 <av4625@gmail.com>
2018-04-03 21:38:20 +0100

    Added tradfri status

--- php/connection.php
+++ php/connection.php
@@ -1,16 +1,16 @@
 <?php
-    // Fields required to connect to the database
-    $host = "localhost";
-    $username = "website";
-    $password = "password";
-    $dbname = "house_temp";
+// Fields required to connect to the database
+$host = "localhost";
+$username = "website";
+$password = "password";
+$dbname = "house_temp";
 
-    // Create connection to MySQL database
-    $connection = new mysqli($host, $username, $password, $dbname);
+// Create connection to MySQL database
+$connection = new mysqli($host, $username, $password, $dbname);
 
-    // Check connection to MySQL database
-    if ($connection->connect_error)
-    {
-        die("Connection to database failed: " . $connection->connect_error);
-    }
+// Check connection to MySQL database
+if ($connection->connect_error)
+{
+    die("Connection to database failed: " . $connection->connect_error);
+}
 ?>
---
+++ php/tradfri_status.php
@@ -0,0 +1,26 @@
+<?php
+// Get the status of the kitchen light
+$kitchen = `coap-client -m get -u "google" -k "dsqtsiIgba2IaccE" "coaps://192.168.0.105:5684/15001/65548" | tail -n 1`;
+$kitchen = trim(preg_replace('/\s+/', ' ', $kitchen));
+
+// Get the status of the living room light
+$living_room = `coap-client -m get -u "google" -k "dsqtsiIgba2IaccE" "coaps://192.168.0.105:5684/15001/65553" | tail -n 1`;
+$living_room = trim(preg_replace('/\s+/', ' ', $living_room));
+
+// Get the status of bedroom light
+$bedroom = `coap-client -m get -u "google" -k "dsqtsiIgba2IaccE" "coaps://192.168.0.105:5684/15001/65570" | tail -n 1`;
+$bedroom = trim(preg_replace('/\s+/', ' ', $bedroom));
+
+// Get the status of the bedroom lamp
+$bedroom_lamp = `coap-client -m get -u "google" -k "dsqtsiIgba2IaccE" "coaps://192.168.0.105:5684/15001/65561" | tail -n 1`;
+$bedroom_lamp = trim(preg_replace('/\s+/', ' ', $bedroom_lamp));
+
+$json_array = (object) [
+    'kitchen' => $kitchen,
+    'living_room' => $living_room,
+    'bedroom' => $bedroom,
+    'bedroom_lamp' => $bedroom_lamp
+];
+
+echo(json_encode($json_array));
+?>
