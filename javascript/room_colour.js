447d3392fbdf3424b96869c84164f75b7df6f861
av4625 <av4625@gmail.com>
2018-05-14 22:11:20 +0100

    Add functionality to show the colour of the bulb

--- javascript/room_colour.js
+++ javascript/room_colour.js
@@ -79,3 +79,101 @@ function is_date_old(date)
 
     return false;
 }
+
+// Set colour of light bulb
+function set_bulb_colour(img_id, rgb_colour)
+{
+    // Load the light bulb image in and redraw it with the new colour
+    var img = new Image();
+
+    img.onload = function()
+    {
+        var canvas = document.createElement('canvas');
+
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        var ctx = canvas.getContext('2d');
+        ctx.drawImage(img, 0, 0);
+
+        var image_data = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var data = image_data.data;
+
+        for (var i = 0; i < data.length; i += 4)
+        {
+            if (data[i + 3] == 0)
+            {
+                continue;
+            }
+
+            // If the pixel is yellow set it to the new colour
+            if (data[i] > 200 && data[i + 2] < 100)
+            {
+                data[i] = rgb_colour[0];
+                data[i + 1] = rgb_colour[1];
+                data[i + 2] = rgb_colour[2];
+            }
+        }
+
+        ctx.putImageData(image_data, 0, 0);
+
+        // Set the image src to the bulb with the new colour
+        $(img_id).attr('src', canvas.toDataURL());
+    }
+
+    img.src = 'images/light_bulb.png';
+}
+
+/**
+* Converts an HSV color value to RGB. Conversion formula
+* adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+* Assumes h, s, and v are contained in the set [0, 1] and
+* returns r, g, and b in the set [0, 255].
+*
+* @param   Number  h       The hue
+* @param   Number  s       The saturation
+* @param   Number  v       The value
+* @return  Array           The RGB representation
+*/
+function hsv_to_rgb(h, s, v)
+{
+    var r, g, b;
+
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+
+    switch (i % 6)
+    {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+
+    return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
+}
+
+// Return true if the arrys are the same else return false
+function are_arrays_equal(array_one, array_two)
+{
+    if (array_one.length != array_two.length)
+    {
+        return false;
+    }
+
+    for (var i = 0; i < array_one.length; i++)
+    {
+        if (array_one[i] != array_two[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
