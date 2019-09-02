860d1ef4a1090d873660f3844843262ecc728ed7
Aaron Vance <av4625@gmail.com>
2019-09-02 22:43:19 +0100

    Add media query for phones, add a check to see if we get data from firebase

--- javascript/firebase.js
+++ javascript/firebase.js
@@ -50,49 +50,70 @@ function listeners()
     var current_main_bedroom_ref = database.ref('main_bedroom/current');
     current_main_bedroom_ref.on('value', function(snapshot)
     {
-        set_temperature('main_bedroom', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('main_bedroom', snapshot.val());
+        }
     });
 
     // Current kitchen temperature listener
     var current_kitchen_ref = database.ref('kitchen/current');
     current_kitchen_ref.on('value', function(snapshot)
     {
-        set_temperature('kitchen', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('kitchen', snapshot.val());
+        }
     });
 
     // Current spare bedroom temperature listener
     var current_spare_bedroom_ref = database.ref('spare_bedroom/current');
     current_spare_bedroom_ref.on('value', function(snapshot)
     {
-        set_temperature('spare_bedroom', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('spare_bedroom', snapshot.val());
+        }
     });
 
     // Current hot press temperature listener
     var current_hot_press_ref = database.ref('hot_press/current');
     current_hot_press_ref.on('value', function(snapshot)
     {
-        set_temperature('hot_press', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('hot_press', snapshot.val());
+        }
     });
 
     // Current dressing room temperature listener
     var current_dressing_room_ref = database.ref('dressing_room/current');
     current_dressing_room_ref.on('value', function(snapshot)
     {
-        set_temperature('dressing_room', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('dressing_room', snapshot.val());
+        }
     });
 
     // Current dining room temperature listener
     var current_dining_room_ref = database.ref('dining_room/current');
     current_dining_room_ref.on('value', function(snapshot)
     {
-        set_temperature('dining_room', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('dining_room', snapshot.val());
+        }
     });
 
     // Current living room temperature listener
     var current_living_room_ref = database.ref('living_room/current');
     current_living_room_ref.on('value', function(snapshot)
     {
-        set_temperature('living_room', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_temperature('living_room', snapshot.val());
+        }
     });
 
     // Main bedroom average temperature from previous week listener
@@ -232,34 +253,49 @@ function listeners()
     var kitchen_light_ref = database.ref('kitchen/kitchen_light');
     kitchen_light_ref.on('value', function(snapshot)
     {
-        set_light('kitchen_light', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_light('kitchen_light', snapshot.val());
+        }
     });
 
     // Living room light listener
     var living_room_light_ref = database.ref('kitchen/living_room_light');
     living_room_light_ref.on('value', function(snapshot)
     {
-        set_light('living_room_light', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_light('living_room_light', snapshot.val());
+        }
     });
 
     // Bedroom light listener
     var bedroom_light_ref = database.ref('bedroom/bedroom_light');
     bedroom_light_ref.on('value', function(snapshot)
     {
-        set_light('bedroom_light', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_light('bedroom_light', snapshot.val());
+        }
     });
 
     // Bedoom lamp listener
     var bedroom_lamp_ref = database.ref('bedroom/bedroom_lamp');
     bedroom_lamp_ref.on('value', function(snapshot)
     {
-        set_light('bedroom_lamp', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_light('bedroom_lamp', snapshot.val());
+        }
     });
 
     // Spare bedoom light listener
     var spare_bedroom_light_ref = database.ref('spare_bedroom/spare_bedroom_light');
     spare_bedroom_light_ref.on('value', function(snapshot)
     {
-        set_light('spare_bedroom_light', snapshot.val());
+        if (snapshot.val() !== null)
+        {
+            set_light('spare_bedroom_light', snapshot.val());
+        }
     });
 }
